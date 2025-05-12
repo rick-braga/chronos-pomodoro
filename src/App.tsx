bf5044ea7c023f47f6326c4aaffee9877fe9b1d5
Rick Braga <rick@exemplo.com>
2025-05-12 19:32:54 -0300

    feat(cycles): criar componente com indicadores do ciclo pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -35,8 +36,7 @@ function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className='formRow'>
