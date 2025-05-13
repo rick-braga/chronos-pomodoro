d6dcd5d7efeb0686af660c9c51635f79a228e8b4
Rick Braga <rick@exemplo.com>
2025-05-13 18:49:33 -0300

    feat(state): implementar estado global da aplicação com useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import Home from './pages/Home';
+import { TaskStateModel } from './models/TaskStateModel';
+import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
 
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 function App() {
-  return <Home />;
+  //criamos o state aqui pq vamos jogar em componentes filhos do App
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
 
 export default App;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import { TaskStateModel } from '../../models/TaskStateModel';
 import MainTemplate from '../../templates/MainTemplate';
 
-function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+function Home(props: HomeProps) {
+  // Home só está recebendo as props porque precisamos enviar pra CountDown e MainForm
+  const { state, setState } = props;
+
   return (
     <MainTemplate>
       <Container>
